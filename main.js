24f0becfcb71f13479d41a9c6d6120b7f3e2ca0a
yesharry <reerohh@gmail.com>
2023-08-03 20:42:39 +0900

    코드 정리

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 "use strict";
 
-const BALLOON_SIZE = 130;
-const BALLOON_COUNT = 30;
-
 const BALLOON_COLOR = [
   "rgba(242, 107, 66, 0.9)",
   "rgba(61, 137, 253, 0.9)",
@@ -11,21 +8,22 @@ const BALLOON_COLOR = [
   "rgba(254, 224, 58, 0.9)",
   "rgba(242, 45, 81, 0.9)",
 ];
+const BALLOON_RADIUS = ["10%", "20%", "30%", "40%", "50%"];
+const BALLOON_SIZE = 130;
+const BALLOON_COUNT = 50;
 
 const field = document.querySelector(".balloon_field");
 const fieldRect = field.getBoundingClientRect();
+field.addEventListener("click", onItemClick);
 
 const house = document.querySelector(".house");
-
-field.addEventListener("click", onItemClick);
 house.addEventListener("click", onHouseClick);
 
-// 풍선을 추가한 뒤 field에 추가
 function init() {
   addItem("balloon", BALLOON_COUNT);
+  addItem("sale", 1);
 }
 
-// balloon click
 function onItemClick(event) {
   const target = event.target;
   if (target.matches(".balloon")) {
@@ -44,17 +42,20 @@ function addItem(className, count) {
   const y2 = fieldRect.height - BALLOON_SIZE;
 
   for (let i = 0; i < count; i++) {
-    const random = Math.floor(Math.random() * (6 - 1) + 1);
-    const item = document.createElement("div");
+    const randomColor = Math.floor(Math.random() * (6 - 1) + 1);
+    const randomRadius = Math.floor(Math.random() * (5 - 1) + 1);
 
+    const item = document.createElement("div");
     item.setAttribute("class", className);
     item.style.position = "absolute";
-    item.style.backgroundColor = BALLOON_COLOR[random];
+    item.style.backgroundColor = BALLOON_COLOR[randomColor];
+    item.style.borderRadius = BALLOON_RADIUS[randomRadius];
 
     const x = randomNumber(x1, x2);
     const y = randomNumber(y1, y2);
     item.style.top = `${y}px`;
     item.style.left = `${x}px`;
+
     field.appendChild(item);
   }
 }
